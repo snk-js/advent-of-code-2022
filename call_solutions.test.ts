1357c0e6a1a8b2cbe749d1565af7f7f4798698b4
juliano <julianoh1909@gmail.com>
2023-02-09 02:39:26 -0300

    day 1 [2/2] accomplished

--- call_solutions.test.ts
+++ call_solutions.test.ts
@@ -1,11 +1,11 @@
-import { getElfCarryingMostCalories } from './day-1/index'
+import { getElfCarryingMostCalories, getTopThreeElfCarryingMostCalories } from './day-1/index'
 
 
 describe('test all solution', () => {
 
     it('should test day 1 solutions', () => {
-        getElfCarryingMostCalories()
-
+        expect(getElfCarryingMostCalories()).toBeGreaterThan(60001)
+        expect(getTopThreeElfCarryingMostCalories()).toBeGreaterThan(200000)
     })
 
 })
--- day-1/index.ts
+++ day-1/index.ts
@@ -1,14 +1,27 @@
 import { readInput } from '../utils/readInput'
 import { feed } from '../utils/feed'
 
-const solution = (input: string) => {
-  return Math.max(...input.split(/\n\n/)
-    .map((w) => w.replace(/\n(?=.*)/g, ','))
-    .map((c: string) => c.split(',')
-      .reduce((acc, calories: string) => acc + Number(calories), 0)))
+
+const formatAndGroupCalories = (input: string) => input.split(/\n\n/)
+  .map((w) => w.replace(/\n(?=.*)/g, ','))
+  .map((c: string) => c.split(',')
+    .reduce((acc, calories: string) => acc + Number(calories), 0))
+
+
+
+const solution_1 = (input: string) => {
+  return Math.max(...formatAndGroupCalories(input))
 }
 
-export const getElfCarryingMostCalories = feed(readInput('day-1'), solution)
+const solution_2 = (input: string) => {
+  return formatAndGroupCalories(input)
+    .sort((a, b) => a > b ? -1 : 1)
+    .slice(0, 3)
+    .reduce((a, b) => a + b, 0)
+}
+
+export const getElfCarryingMostCalories = feed(readInput('day-1'), solution_1)
+export const getTopThreeElfCarryingMostCalories = feed(readInput('day-1'), solution_2)
 
 
 
