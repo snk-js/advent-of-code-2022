5697af186c18a6acb57358de497c1429a1944c07
juliano <julianoh1909@gmail.com>
2023-02-11 21:48:39 -0300

    day 2 [2/2] accomplished

--- call_solutions.test.ts
+++ call_solutions.test.ts
@@ -1,15 +1,15 @@
-import { countPoints } from './day-2/index'
+import { countPoints, countPointsSecondStrategy } from './day-2/index'
 import { getElfCarryingMostCalories, getTopThreeElfCarryingMostCalories } from './day-1/index'
 
 
 describe('test all solution', () => {
-
     it('should test day 1 solutions', () => {
         expect(getElfCarryingMostCalories()).toBeGreaterThan(60001)
         expect(getTopThreeElfCarryingMostCalories()).toBeGreaterThan(200000)
     })
     it('should test day 2 solutions', () => {
-        expect(countPoints()).toBe(13052)
+        expect(countPoints()).toBeGreaterThan(13000)
+        expect(countPointsSecondStrategy()).toBeGreaterThan(13600)
     })
 
 })
--- day-2/index.ts
+++ day-2/index.ts
@@ -12,7 +12,7 @@ const choice_points = {
     Z: 3,
 } as const
 
-const table = {
+const winner = {
     X: 'C',
     Y: 'A',
     Z: 'B',
@@ -20,6 +20,16 @@ const table = {
     A: 'Z',
     B: 'X',
 } as const
+
+type AllPossibleChoicesFromKeys = keyof typeof winner
+type AllPossibleChoicesFromValues = typeof winner[AllPossibleChoicesFromKeys]
+type Losers<T extends keyof Choices> = Record<Exclude<AllPossibleChoicesFromValues, T>, AllPossibleChoicesFromKeys>;
+const reducerObj = {} as Losers<keyof Choices>;
+
+const losers = Object.entries(winner).reduce((acc, [key, value]) => {
+    acc[value as AllPossibleChoicesFromValues] = key as AllPossibleChoicesFromKeys;
+    return acc;
+}, reducerObj);
 
 const my = ['X', 'Y', 'Z'] as const;
 const their = ['A', 'B', 'C'] as const;
@@ -32,31 +42,44 @@ type Args = {
     theirChoice: TheirPossibleChoices
 };
 
+type Choices = [TheirPossibleChoices, MyPossibleChoices]
+
 // this is a nice type I discored talking to chatGPT
 // is the result of an operation that builds
 // all possible values of the combination of two union types
 type PossibleStrategies = (Args & { string: string })['string'];
 
-const getPoints = ({ myChoice, theirChoice }: Args) => getResult({ myChoice, theirChoice }) + choice_points[myChoice]
-
-const getResult = ({ myChoice, theirChoice }: Args): typeof result_points[number] =>
-    (table[myChoice] === theirChoice && 6) ||
-    ((table[theirChoice] === myChoice && 0) === 0 ? 0 : 3);
+const getPoints =
+    ({ myChoice, theirChoice }: Args) =>
+        getResult({ myChoice, theirChoice }) + choice_points[myChoice]
 
 
-const solution_1 = (input: string) => format(input).reduce((acc, choices: PossibleStrategies) => {
-    const [theirChoice, myChoice] = choices.split(" ") as [TheirPossibleChoices, MyPossibleChoices]
-    const points = getPoints({ myChoice, theirChoice })
-    return acc + points
-}, 0)
-
-
-const solution_2 = (input: string) => {
-
+const reduce = (input: string, convert: boolean) => {
+    return format(input).reduce((acc, choices: PossibleStrategies) => {
+        const [theirChoice, myChoice] = choices.split(" ") as Choices
+        const points = getPoints({ theirChoice, myChoice: convert ? changeStrategy({ myChoice, theirChoice }) : myChoice })
+        return acc + points
+    }, 0)
 }
 
+
+
+const getResult = ({ myChoice, theirChoice }: Args): typeof result_points[number] =>
+    (winner[myChoice] === theirChoice && 6) ||
+    ((winner[theirChoice] === myChoice && 0) === 0 ? 0 : 3);
+const getSolution = (input: string, convert: boolean = false) =>
+    reduce(input, convert)
+
+
+// strategy adopted to solve solution 2
+const changeStrategy = ({ myChoice, theirChoice }: Args): MyPossibleChoices => {
+    if (myChoice === 'X') return winner[theirChoice];
+    if (myChoice === 'Z') return losers[theirChoice] as MyPossibleChoices;
+    return my[their.indexOf(theirChoice)];
+}
+
+const solution_1 = (input: string) => getSolution(input);
+const solution_2 = (input: string) => getSolution(input, true)
+
 export const countPoints = feed(readInput('day-2'), solution_1)
-export const _ = feed(readInput('day-2'), solution_2)
-
-
-
+export const countPointsSecondStrategy = feed(readInput('day-2'), solution_2)
